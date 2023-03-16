ab6a84b2ae93fc422c3e8ba16784ea09328226e5
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-16 09:54:22 +0530

    greeting message for line comparison

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using linecomparisonproblem;
+
+namespace lineComparisonproblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            //CalculateLength.lengthcal();
+             uc2.lengthcheck();
+            comparision.Display();
+
+        }
+    }
+}
